1208bf4f38b1532431914adbdaac3f303a0c83f5
TKhai55 <thienkhaiak5555@gmail.com>
2023-12-16 10:16:29 +0700

    done bookedSeat

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -80,6 +80,19 @@ const actions = {
     }
   },
 
+  [MutationTypes.GET_SCHEDULE_DETAIL]: async (
+    { commit }: { commit: any },
+    payload: any
+  ) => {
+    payload = turnOnDevMode(payload);
+    const response = await sendGetNoToken(`/Schedule/${payload.id}`);
+    if (response) {
+      return response;
+    } else {
+      return null;
+    }
+  },
+
   [MutationTypes.GET_MOVIE_SCHEDULE]: async (
     { commit }: { commit: any },
     payload: any
