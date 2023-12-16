2b6293ad89f8e9f4b6e6ae9d555460a5720cd3b0
TKhai55 <thienkhaiak5555@gmail.com>
2023-12-16 11:55:01 +0700

    fix upcoming

--- src/components/top-featured-movie/top-featured-movie.vue
+++ src/components/top-featured-movie/top-featured-movie.vue
@@ -3,25 +3,26 @@
 <script lang="ts" src="./top-featured-movie.ts"></script>
 
 <template>
+    <VideoIframe ref="video-iframe-component" :videoUrl="selectedMovieTrailer" @closeIframe="handleCloseIframe"/>
+
     <div class="top-movie-container">
         <i class="bi bi-film"></i>
-        <p>Checkout Movies</p>
+        <p>New Movies</p>
         <div class="title-container">
-            <p class="title">Top Featured Movies</p>
-            <p class="description">Phasellus non cursus ligula, sed mattis urna. Aenean ac tor gravida, volutpat quam eget, consequat elit.</p>
+            <p class="title">Upcoming Movie</p>
         </div>
 
         <div class="d-flex ">
             <div>
                 <Splide class="mx-2" :options="splideSlideOptions" ref="thumbs" aria-label="My Favorite Images">
-                    <SplideSlide :key="index" v-for="(item, index) in movies" class="d-flex flex-column align-items-center">
+                    <SplideSlide :key="index" v-for="(item, index) in allMovies" class="d-flex flex-column align-items-center">
                         <img :src="item.image" class="movie-image"/>
                         <div class="infor-container">
-                            <p class="infor-title cursor_pointer">{{ item.name }}</p>
-                            <div class="d-flex" style="gap: 20px">
+                            <p class="infor-title cursor_pointer" @click="handleNavigateDetailMovie(item.id)">{{ item.name }}</p>
+                            <div class="d-flex flex-row justify-content-between" style="gap: 20px">
                                 <div class="d-flex">
                                     <i class="bi bi-tags-fill color-icon" style="margin-right: 5px"></i>
-                                    <p>{{ item.category.join(', ') }}</p>
+                                    <p>{{ item.genre.join(', ') }}</p>
                                 </div>
 
                                 <div class="d-flex">
@@ -30,9 +31,9 @@
                                 </div>
                             </div>
 
-                            <div class="d-flex" style="gap: 20px">
-                                <button class="btn-action">Watch Trailer</button>
-                                <button class="btn-action">Get Ticket</button>
+                            <div class="d-flex" style="gap: 20px ">
+                                <button class="btn-action" @click="handleOpenTrailerPopup(item.trailer)">Watch Trailer</button>
+                                <button class="btn-action"  @click="handleNavigateDetailMovie(item.id)">Get Ticket</button>
                             </div>
                         </div>
                     </SplideSlide>
