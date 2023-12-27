8a891db4bac4e3b9ac6f6a66f139208fd5eaf88e
nhtrungkien <trungkieenn0202@gmail.com>
2023-12-28 01:21:01 +0700

    contact us

---
+++ src/views/contactUs/contactUs.ts
@@ -0,0 +1,12 @@
+import { Vue, Options } from 'vue-class-component'
+import Header from '@/components/header/header.vue'
+
+Options({
+    components: {
+        Header
+    }
+})
+
+export default class ContactUs extends Vue {
+    
+}
